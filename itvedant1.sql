50cef9d54859f8f0f63ed0fb0573cbe4c5b17c26
Sanket Chaskar <sanketchaskar4085@gmail.com>
2024-03-08 17:27:13 +0530

    'sqlworkbench'

---
+++ itvedant1.sql
@@ -0,0 +1,11 @@
+use sanket1;
+create table itvedant1 (studid int primary  , studname varchar(300), coursename varchar(300) default 'dse');
+insert into itvedant1 value (1, 'sanket', 'dse');
+desc itvedant1;
+select * from itvedant1;
+insert into itvedant1 value (2,'saud',default);
+alter table itvedant1 modify studid int primary key;
+alter table itvedant1 modify studid int auto_increment;
+insert into itvedant1 (studname) values ('omkar'),('sunny'),('siddhu');
+update itvedant1 set studname='mandar' where studid=4;
+delete from itvedant1 where studid=5;
